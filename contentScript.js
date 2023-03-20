0eece5be44251748d523313f4345c6b095ac7152
José Pascual <jose.pascual@omniaccess.com>
2023-03-20 11:26:33 +0100

    feat: version 1.0

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
-let TW_TAB_ACTIVE = '';
-let TW_TAB_ACTIVE_WORDS = ['para-ti'];
+let TW_TAB_ACTIVE = false;
+let TW_NOT_INTERESTED_WORDS = ['no me interesa', 'not interested'];
 
 (async () => {
     // check every 5 seconds if the element is available
@@ -18,9 +18,7 @@ let TW_TAB_ACTIVE_WORDS = ['para-ti'];
 
     const setTabStatusToBody = activeTab => {
         const body = document.querySelector('body')
-        if (body) {
-            body.setAttribute('data-shity-btn', activeTab)
-        }
+        if (body) body.setAttribute('data-shity-btn', activeTab)
     }
 
     const createShityBtn = (tweet) => {
@@ -38,124 +36,45 @@ let TW_TAB_ACTIVE_WORDS = ['para-ti'];
     // wait for the element to be available
     await waitForElm('[role="region"] article');
 
-    // // cookies
-    // const cookiesRaw = `; ${document.cookie}`;
-    // const cookies = cookiesRaw.split(`; `).filter(c => c).reduce((acc, val) => {
-    //     const key = val.split('=')[0]
-    //     const value = val.split(`${key}=`)[1]
-
-    //     acc[key] = value
-
-    //     return acc
-    // }, {})
-
-    // Get Scripts
-    /*
-    const scripts = document.querySelectorAll('script[type="text/javascript"]:not([src])')
-    let contentScripts = null
-    if (scripts) {
-        scripts.forEach(script => {
-            if (script.innerText.includes('window.__INITIAL_STATE__') && script.innerText.includes('window.__META_DATA__')) {
-                contentScripts = script.innerText.split('};').filter(e => e).reduce((acc, value) => {
-                    const key = value.split('=')[0].replaceAll('window.', '')
-                    const content = value.split('=')[1] + '}'
-
-                    acc[key] = JSON.parse(content)
-
-                    return acc
-                }, {})
-            }
-        })
-    }
-    */
-
-    // Add element on [role="navigation"]
-    const menu = document.querySelector('[role="navigation"]')
-    if (menu) {
-        const buttonMenu = document.createElement('button')
-        buttonMenu.classList.add('extensionConfigBtn')
-        buttonMenu.innerHTML = '<span class="icon">💩</span><span class="text">Extension config</span>'
-        menu.appendChild(buttonMenu)
-    }
-
-    // Add Modal
-    const body = document.querySelector('body')
-    if (body) {
-        body.setAttribute('data-extension-config', false)
-        const modalDiv = document.createElement('div')
-        modalDiv.classList.add('extensionModal')
-        modalDiv.innerHTML = `<div class="modalCard">
-            <header>
-                <h3>Extension Config</h3>
-                <button class="closeExtensionModal">
-                    <svg class="closeIcon" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0" viewBox="0 0 320.591 320.591" style="enable-background: new 0 0 512 512" xml:space="preserve"><path d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"></path><path d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"></path></svg>
-                </button>
-            </header>
-            <div class="modalContent">
-                <div class="disclaimer">
-                Lorem Ipsum
-                </div>
-                <div class="actions">actions</div>
-            </div>
-            <div class="modalFooter">
-                <p class="made">Made with Love by #TOP and José</p>
-            </div>
-        </div>`
-        body.appendChild(modalDiv)
-    }
-
-    // get all tweets on the view
+    // First Load set shitty btn on all tweets loaded
     const tweets = document.querySelectorAll('[role="region"] article')
-
-    // Add shit emoji button in every tweet
     tweets.forEach(tweet => createShityBtn(tweet))
 
-    // every second check if are new tweets
+    // Interval every 300ms
     const interval = setInterval(() => {
-        TW_TAB_ACTIVE = document.querySelector('[role="tablist"] [role="tab"][aria-selected="true"]').innerText.toLowerCase().split(' ').join('-')
+        // Get active tab
+        const firstTab = document.querySelector('[role="tablist"] [role="tab"]:nth-child(1)')
+        TW_TAB_ACTIVE = firstTab && firstTab.getAttribute('aria-selected') === 'true'
+        setTabStatusToBody(TW_TAB_ACTIVE)
 
-        setTabStatusToBody(TW_TAB_ACTIVE_WORDS.includes(TW_TAB_ACTIVE))
-        // get all tweets on the view that don't have the data-tweet attribute
+        // Get all tweets on the view that don't have the data-tweet attribute
         const newTweets = document.querySelectorAll('[role="region"] article:not([data-tweet])')
-        if(newTweets.length === 0) return
-        newTweets.forEach(tweet => createShityBtn(tweet))
-    }, 300)
+        if (newTweets && newTweets.length > 0) newTweets.forEach(tweet => createShityBtn(tweet))
+    }, 250)
 
-    // add listener to the button
+    // Add listeners to the injected shity button
     document.addEventListener('click', (e) => {
         if (!e.target.classList.contains('shitBtn')) return
         const tweet = e.target.closest('article')
+
+        // Trigger click on dropdown
         const btnDropdown = tweet.querySelector('[aria-haspopup="menu"][role="button"]')
         btnDropdown.click()
 
+        // wait for the dropdown to be available
         setTimeout(() => {
             const dropdown = document.querySelector('[data-testid="Dropdown"]')
             if (!dropdown) return
             const itemsDropdown = dropdown.querySelectorAll('div[role="menuitem"]')
             if (!itemsDropdown.length) return
             itemsDropdown.forEach(item => {
-                if (item.innerText.toLowerCase().includes('no me interesa')) item.click()
+                const word = item.innerText.toLowerCase()
+                if (TW_NOT_INTERESTED_WORDS.find(w => word.includes(w))) item.click()
             })
         }, 25)
     })
 
-    const extensionConfigBtn = document.querySelector('.extensionConfigBtn')
-    if (extensionConfigBtn) {
-        extensionConfigBtn.addEventListener('click', () => {
-            const body = document.querySelector('body')
-            body.setAttribute('data-extension-config', true)
-        })
-    }
-
-    const extensionModal = document.querySelector('.extensionModal')
-    if (extensionModal) {
-        extensionModal.querySelector('button.closeExtensionModal').addEventListener('click', () => {
-            const body = document.querySelector('body')
-            body.setAttribute('data-extension-config', false)
-        })
-    }
-
-    // destroy interval when user leave the page
+    // Destroy interval when user leave the page
     window.addEventListener('beforeunload', () => {
         clearInterval(interval)
     })
